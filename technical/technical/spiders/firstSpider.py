b6893eaa3ccc972709350588a2570a9cf5aa2b3d
Brendan Howell <bhowell841@gmail.com>
2019-07-01 22:32:04 -0400

    added multiple page scrapes

--- technical/technical/spiders/firstSpider.py
+++ technical/technical/spiders/firstSpider.py
@@ -11,13 +11,14 @@ from ..items import AmazonItem
 
 class FirstSpider(scrapy.Spider):
     name = "firstSpider"
-   
+    pageNumber = 2
     start_urls = [
         'https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/product-reviews/B0792MJLNM/ref=cm_cr_getr_d_paging_btm_next_8?ie=UTF8&reviewerType=all_reviews&pageNumber=1'
     ]
     
     def parse(self, response):
         items = AmazonItem()
+        
         
         #review_id = response.css('').css('::text').extract()
         review_title = response.css('.a-text-bold span').css('::text').extract()
@@ -30,8 +31,10 @@ class FirstSpider(scrapy.Spider):
         items['review_date'] = review_date
         items['review_stars'] = review_stars
         items['review_text'] = review_text
-        
+            
         yield items
     
-    
-    
+        next_page = 'https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/product-reviews/B0792MJLNM/ref=cm_cr_getr_d_paging_btm_next_8?ie=UTF8&reviewerType=all_reviews&pageNumber=' + str(FirstSpider.pageNumber)
+        if FirstSpider.pageNumber <= 8:
+            FirstSpider.pageNumber += 1
+            yield response.follow(next_page, callback = self.parse)
