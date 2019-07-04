94e9f74037c20f4cb6bb94e0f561546385932693
Brendan Howell <bhowell841@gmail.com>
2019-07-04 16:14:49 -0400

    product review is done

--- technical/technical/spiders/secondSpider.py
+++ technical/technical/spiders/secondSpider.py
@@ -11,7 +11,10 @@ from ..items import AmazonItem
 class SecondSpider(scrapy.Spider):
     name = "secondSpider"
     
-    pageNumber = 2
+    custom_settings = {
+        'COLLECTION_NAME' : 'reviews_tb'
+    }
+    
     start_urls = [
         'https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/product-reviews/B0792MJLNM/ref=cm_cr_getr_d_paging_btm_next_8?ie=UTF8&reviewerType=all_reviews&pageNumber=1'
     ]
